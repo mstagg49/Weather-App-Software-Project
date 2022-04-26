e4212c8e0a018105021a8feb23bdc4f894e4ce54
mstagg49 <mstagg49@gmail.com>
2022-04-25 20:05:22 -0400

    Version for MVP Demonstration

--- Weather_API_Call.py
+++ Weather_API_Call.py
@@ -16,18 +16,10 @@ while True:
         zc_location_name = response.json()['name']
         zc_temp = response.json()['main']['temp']  # need to convert from Kelvin to Fahrenheit
         zc_condition = response.json()['weather'][0]['description']
-        to_be_written = str(zc_temp) + ", " + str(zc_condition) + ", " + str(zc_location_name)
+        zc_icon = response.json()['weather'][0]['icon'] # returns icon ID
+        to_be_written = str(zc_temp) + ", " + str(zc_condition) + ", " + str(zc_location_name) + ", " + str(zc_icon)
         txt_file = open('get-API-data-service.txt', 'w')
         txt_file.write(to_be_written)
         txt_file.close()
         time.sleep(5)
 
-
-response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?zip={zipcode_request},US&appid={my_API_Key}')
-# TEMPERATURE FROM API IS RETURNED IN KELVIN! NEEDS TO BE CONVERTED
-
-print(response.json())
-loc = response.json()['name'] # returns name of place
-print(loc)
-print(response.json()['main']['temp']) # returns temps(high/low/current), humidity
-print(response.json()['weather'][0]['description'])
--- Weather_GUI.py
+++ Weather_GUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
+from PIL import ImageTk, Image
 import time
-from functools import partial
 
 
 def find_weather_by_zipcode(zipcode):
@@ -11,6 +11,8 @@ def find_weather_by_zipcode(zipcode):
         Temp (High / Low / Current)
         Description of conditions (Cloudy, Rainy, etc)
     """
+    global weather_img
+
     # Send zipcode to zipcode_verify.py
     txt_file = open('zipcode-verif-service.txt', 'w')
     txt_file.write(str(zipcode))  # Converts to String
@@ -27,8 +29,8 @@ def find_weather_by_zipcode(zipcode):
 
     # If False, write error message
     if txt_file_content == "False":
-        false_label = Label(root, text='This is not a valid zipcode. Please enter a new zipcode.')
-        false_label.pack()
+        invalid_zip_text = 'This is not a valid zipcode. Please enter a new zipcode.'
+        invalid_zip_label.config(text=invalid_zip_text)
         return
 
     # Else, get information for that zipcode!
@@ -48,31 +50,60 @@ def find_weather_by_zipcode(zipcode):
     print(weather_data)
 
     # Separate data by delimiter (comma)
-    # data order: temp, condition, location
+    # data order: temp, condition, location, icon
     weather_data = weather_data.split(', ')
 
-    temp_label = Label(root, text=weather_data[0])
-    temp_label.pack()
-    cond_label = Label(root, text=weather_data[1])
-    cond_label.pack()
-    loc_label = Label(root, text=weather_data[2])
-    loc_label.pack()
+    # Display requested weather data
+    icon_id = weather_data[3]
+    # construct file path for Weather Icon
+    icon_path = 'weather_icons\\' + str(icon_id) + '.jpg'
+    # load image into window
+    weather_img = ImageTk.PhotoImage(Image.open(icon_path))
+
+    # Display data:
+    # Note - putting the label creation outside the function made this work
+    #   you're only configuring the label in the function, not creating a new one every time
+    # Location
+    loc_label.config(text=weather_data[2])
+    # Weather Condition
+    cond_label.config(text=weather_data[1])
+    # Temperature
+    c_temp = round((float(weather_data[0])-273.15) * (9/5) + 32)
+    temp_label.config(text=str(c_temp) + ' F')
+    # Icon
+    icon_label.config(image=weather_img)
+
+    # Display hyperlink
 
 
 
-
-
+# Initializing Tkinter window
 root = Tk()
+root.geometry("300x300")
+root.config(background="blue")
 root.title("Weather App")
 
-# Create a place for image to go
-#canvas = Canvas(root, width=300, height=400)
-#canvas.pack()
+# Creating labels for data display
+# Location
+loc_label = Label(root)
+loc_label.pack()
+# Weather Condition
+cond_label = Label(root)
+cond_label.pack()
+# Temperature
+temp_label = Label(root)
+temp_label.pack()
+# Icon
+icon_label = Label(root)
+icon_label.pack()
+
+# Frame for invalid zipcode
+invalid_zip_label = Label(root)
+invalid_zip_label.pack()
 
 # Create a search bar
 search_bar = Entry(root, borderwidth=5)
 search_bar.pack()
-
 
 # Create a button to execute search for string in search bar
 button_text = "Search zipcode"
@@ -83,5 +114,9 @@ button.pack()
 button_text = "Exit window"
 button = Button(root, width=30, text=button_text, command=root.destroy)
 button.pack()
+
+# Create label where hyperlink will go
+hyperlink_label = Label(root)
+hyperlink_label.pack()
 
 root.mainloop()
